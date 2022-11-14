8bec1d3169c9eb0d8d174d7160dfed40232f5f12
gdany91 <you@example.comdgarofalo52@gmail.com>
2022-11-14 11:25:28 +0100

    reverse

--- main.js
+++ main.js
@@ -2,27 +2,29 @@
 const item = document.getElementsByClassName('item');
 console.log(item);
 
-let activeItem = 0;
-
-
+let activeItemUp = 0;
+;
+//scorrimento su
 const next = document.querySelector('.arrow');
 
 next.addEventListener('click', 
 
     function(){
         
-        if(activeItem < item.length - 1){
+        if(activeItemUp < item.length - 1){
 
-            item[activeItem].classList.remove['active'];
-            item[activeItem].classList.add['hidden'];
+            item[activeItemUp].classList.remove('active');
+            item[activeItemUp].classList.add('hidden');
 
-            activeItem++;
+            activeItemUp++;
 
-            item[activeItem].classList.add['active'];
+            console.log(activeItemUp);
+            item[activeItemUp].classList.remove('hidden');
+            item[activeItemUp].classList.add('active');
 
-            if (activeItem === item.length - 1) {
+            if (activeItemUp === item.length - 1) {
 
-                 next.classList.remove['hidden'];
+                 next.classList.add('hidden');
             }
 
 
@@ -32,3 +34,42 @@ next.addEventListener('click',
 
 )
 
+//scorrimento giu
+ 
+ const previous = document.querySelector('.reverse_arrow');
+
+ previous.addEventListener('click', 
+
+    function(){
+        
+         
+        if (activeItemUp <= item.length - 1){
+            
+            item[activeItemUp].classList.remove('active');
+            item[activeItemUp].classList.add('hidden');
+
+            activeItemUp--;
+
+            console.log(activeItemUp);
+
+            item[activeItemUp].classList.remove('hidden');
+            item[activeItemUp].classList.add('active');
+ 
+            if (activeItemUp === 0) {
+
+                previous.classList.add('hidden');
+
+
+            }
+
+            else {
+            previous.classList.add('active');
+            previous.classList.remove('hidden');
+            }
+ 
+
+         }
+
+    }
+
+) 
